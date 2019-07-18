bf1b8cd3f03519ff70be23752a5b42e6f05c1552
horsicq <horsicq@gmail.com>
2019-07-18 18:28:07 +0200

    Fix

--- StringsPlugin/QtPlugin.h
+++ StringsPlugin/QtPlugin.h
@@ -25,8 +25,8 @@
 //#include "PluginMainWindow.h"
 
 #define plugin_name "Strings"
-#define plugin_version 3 // 0.03
-#define plugin_version_str "0.03"
+#define plugin_version 4 // 0.04
+#define plugin_version_str "0.04"
 
 namespace QtPlugin
 {
